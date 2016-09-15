105629c8dbc22dd9c62c0184b59806fcbe7309a2
Abizer Nasir <abizern@abizern.org>
2016-09-15 16:30:00 +0100

    Update to Swift 3.0 and Xcode 8

--- LoggingPrint.swift
+++ LoggingPrint.swift
@@ -5,38 +5,28 @@
 import Foundation
 
 /**
-Prints the filename, function name, line number and textual representation of `object` and a newline character into
-the standard output if the build setting for "Other Swift Flags" defines `-D DEBUG`.
+ Prints the filename, function name, line number and textual representation of `object` and a newline character into
+ the standard output if the build setting for "Active Complilation Conditions" (SWIFT_ACTIVE_COMPILATION_CONDITIONS) defines `DEBUG`.
 
-The current thread is a prefix on the output. <UI> for the main thread, <BG> for anything else.
+ The current thread is a prefix on the output. <UI> for the main thread, <BG> for anything else.
 
-Only the first parameter needs to be passed to this funtion.
+ Only the first parameter needs to be passed to this funtion.
 
-The textual representation is obtained from the `object` using its protocol conformances, in the following
-order of preference: `CustomDebugStringConvertible` and `CustomStringConvertible`. Do not overload this function for 
-your type. Instead, adopt one of the protocols mentioned above.
+ The textual representation is obtained from the `object` using `String(reflecting:)` which works for _any_ type.
+ To provide a custom format for the output make your object conform to `CustomDebugStringConvertible` and provide your format in
+ the `debugDescription` parameter.
 
-:param: object   The object whose textual representation will be printed. If this is an expression, it is lazily evaluated.
-:param: file     The name of the file, defaults to the current file without the ".swift" extension.
-:param: function The name of the function, defaults to the function within which the call is made.
-:param: line     The line number, defaults to the line number within the file that the call is made.
-*/
-func loggingPrint<T>(@autoclosure object: () -> T, _ file: String = #file, _ function: String = #function, _ line: Int = #line) {
+ :param: object   The object whose textual representation will be printed. If this is an expression, it is lazily evaluated.
+ :param: file     The name of the file, defaults to the current file without the ".swift" extension.
+ :param: function The name of the function, defaults to the function within which the call is made.
+ :param: line     The line number, defaults to the line number within the file that the call is made.
+ */
+func loggingPrint<T>(_ object: @autoclosure () -> T, _ file: String = #file, _ function: String = #function, _ line: Int = #line) {
     #if DEBUG
         let value = object()
-        let stringRepresentation: String
-
-        if let value = value as? CustomDebugStringConvertible {
-            stringRepresentation = value.debugDescription
-        } else if let value = value as? CustomStringConvertible {
-            stringRepresentation = value.description
-        } else {
-            fatalError("loggingPrint only works for values that conform to CustomDebugStringConvertible or CustomStringConvertible")
-        }
-
         let fileURL = NSURL(string: file)?.lastPathComponent ?? "Unknown file"
-        let queue = NSThread.isMainThread() ? "UI" : "BG"
+        let queue = Thread.isMainThread ? "UI" : "BG"
 
-        print("<\(queue)> \(fileURL) \(function)[\(line)]: " + stringRepresentation)
+        print("<\(queue)> \(fileURL) \(function)[\(line)]: " + String(reflecting: value))
     #endif
 }
--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # LoggingPrint
 
-A Swift 2 convenience function for printing to the console only when `-D DEBUG`
-is defined in the _Other Swift Flags_ setting.
+A Swift . convenience function for printing to the console only when the build setting for "Active Complilation
+Conditions" (SWIFT_ACTIVE_COMPILATION_CONDITIONS) defines `DEBUG`
 
-The textual representation is obtained using protocol conformance, in the
-following order of preference: `CustomDebugStringConvertible` and
-`CustomStringConvertible`. Do not overload this function for your type. Instead,
-adopt one of these protocols.
+The textual representation is obtained from the `object` using `String(reflecting:)` which works for _any_ type. To
+provide a custom format for the output make your object conform to `CustomDebugStringConvertible` and provide your
+format in the `debugDescription` parameter.
 
 Through the magic of default function parameter values, the output contains:
 
@@ -15,20 +14,23 @@ Through the magic of default function parameter values, the output contains:
 - The name of the function
 - The line number where the print statement is located.
 
+## Requirements
+
+The latest version requires Swift 3.0 and Xcode 8.
+
 ## Usage
 
 The same way as you would use a `print()` statement. 
 
 ## Installation
 
-Just add the file to your project, and define `-D DEBUG`
-in your project's _Other Swift Flags_ setting.
+Just add the file to your project, and define `DEBUG` in your project's _Active Complilation Conditions_ setting.
 
 If you are using Carthage add this to your Cartfile
 
 ``` shell
 
-github "JungleCandy/LoggingPrint"
+github "JungleCandy/LoggingPrint" >= 2.0
 
 ```
 
